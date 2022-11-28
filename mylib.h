1a4f068d42bae16038ff2e556890eca804e5d0fe
UIT-20521633 <116575369+UIT-20521633@users.noreply.github.com>
2022-11-28 20:31:45 +0700

    Add files via upload

---
+++ mylib.h
@@ -0,0 +1,64 @@
+﻿
+#pragma once // tránh đụng độ thư viện khi gọi chồng file lên nhau
+#include <stdio.h>
+#include <conio.h>
+#include<ctime> /* thư viện hỗ trợ về thời gian thực */
+#include "windows.h" // thư viện này bá đạo lắm nhé - chứa nhiều đồ chơi nek - cứ tìm hiểu dần dần s
+//======= lấy tọa độ x của con trỏ hiện tại =============
+#define KEY_NONE	-1
+int wherex()
+{
+	CONSOLE_SCREEN_BUFFER_INFO coninfo;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
+	return coninfo.dwCursorPosition.X;
+}
+int wherey()
+{
+	CONSOLE_SCREEN_BUFFER_INFO coninfo;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
+	return coninfo.dwCursorPosition.Y;
+}
+//============== dịch con trỏ hiện tại đến điểm có tọa độ (x,y) ==========
+void gotoXY(int column, int line)
+{
+	COORD coord;
+	coord.X = column;
+	coord.Y = line;
+	SetConsoleCursorPosition(
+		GetStdHandle(STD_OUTPUT_HANDLE),
+		coord
+	);
+}
+//============= đặt màu cho chữ =========
+void SetColor(WORD color)
+{
+	HANDLE hConsoleOutput;
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+	GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
+
+	WORD wAttributes = screen_buffer_info.wAttributes;
+	color &= 0x000f;
+	wAttributes &= 0xfff0;
+	wAttributes |= color;
+
+	SetConsoleTextAttribute(hConsoleOutput, wAttributes);
+}
+//============== làm ẩn trỏ chuột ===========
+void ShowCur(bool CursorVisibility)
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cursor = { 1, CursorVisibility };
+	SetConsoleCursorInfo(handle, &cursor);
+}
+//======= trả về mã phím người dùng bấm =========
+
+void textcolor(int x)
+{
+	HANDLE mau;
+	mau = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(mau, x);
+}
+
+
